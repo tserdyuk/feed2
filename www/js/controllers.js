0cf86414cbfa21d0f6210b73db87f035a377bf0f
Taras Serdyuk <taras@techburg.io>
2015-11-02 18:01:08 +0300

    read items dupes fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('Main', function($scope) {
+.controller('Main', function($scope, $window) {
   $scope.config = config
+  $scope.read = JSON.parse($window.localStorage.read || '[]')
 })
 
 .controller('DashCtrl', function($scope, $http) {
@@ -20,21 +21,17 @@ angular.module('starter.controllers', [])
   $scope.title = $stateParams.title
   $http.get(config.api.item + $stateParams.id).then(function(response) {
     $scope.content = response.data.body
-    var read = JSON.parse($window.localStorage.read || '[]')
-    if (!read.find(function(item) { return item.id == $stateParams.id })) {
+    if (!$scope.read.find(function(item) {
+      return item.id == $stateParams.id
+    })) {
       var item = {}
       item[config.item.title] = $stateParams.title
       item[config.item.id] = $stateParams.id
-      read.unshift(item)
-      $window.localStorage.read = JSON.stringify(read)
+      $scope.read.unshift(item)
+      $window.localStorage.read = JSON.stringify($scope.read)
     }
   })
 })
 
-.controller('AccountCtrl', function($scope, $window) {
-  function update() {
-    $scope.items = JSON.parse($window.localStorage.read || '[]')
-  }
-  update()
-  $scope.$on('$ionicView.enter', update)
+.controller('AccountCtrl', function($scope) {
 })
