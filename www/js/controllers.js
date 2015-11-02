f98672a78faeb4a8617de545a04a25bf5e28458c
Taras Serdyuk <taras@techburg.io>
2015-11-02 17:37:40 +0300

    saving read items

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,15 +16,25 @@ angular.module('starter.controllers', [])
   })
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, $http) {
+.controller('ChatDetailCtrl', function($scope, $stateParams, $http, $window) {
   $scope.title = $stateParams.title
   $http.get(config.api.item + $stateParams.id).then(function(response) {
     $scope.content = response.data.body
+    var read = JSON.parse($window.localStorage.read || '[]')
+    if (!read.find(function(item) { return item.id == $stateParams.id })) {
+      var item = {}
+      item[config.item.title] = $stateParams.title
+      item[config.item.id] = $stateParams.id
+      read.unshift(item)
+      $window.localStorage.read = JSON.stringify(read)
+    }
   })
 })
 
-.controller('AccountCtrl', function($scope, $http) {
-  $http.get(config.api.all).then(function(response) {
-    $scope.items = response.data.items
-  })
+.controller('AccountCtrl', function($scope, $window) {
+  function update() {
+    $scope.items = JSON.parse($window.localStorage.read || '[]')
+  }
+  update()
+  $scope.$on('$ionicView.enter', update)
 })
