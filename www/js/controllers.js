8dc0bd4e638817a8114452b245f16cc5fb2ebdad
Taras Serdyuk <taras@techburg.io>
2015-11-04 15:14:31 +0300

    fixed load all filtration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,30 @@ angular.module('starter.controllers', [])
 
 .controller('Main', function($scope, $window, $http) {
   $scope.config = config
-  $scope.model = {}
-  if ($window.localStorage.all) {
-    $scope.model.all = JSON.parse($window.localStorage.all)
-  } else {
-    $http.get(config.api.all).then(function(response) {
-      $scope.model.all = response.data.items
-      $window.localStorage.all = JSON.stringify($scope.model.all)
+  $scope.model = {
+    read: JSON.parse($window.localStorage.read || '[]'),
+    selected: JSON.parse($window.localStorage.selected || '[]')
+  }
+  $scope.filterAll = function(items) {
+    var read = {}
+    $scope.model.read.forEach(function(item) {
+      read[item.id] = true
+    })
+    var time = new Date().getTime()
+    $scope.model.all = items.filter(function(item) {
+      return config.item.getDate(item).getTime() < time
+    }).filter(function(item) {
+      return !read[item.id]
     })
   }
-  $scope.model.read = JSON.parse($window.localStorage.read || '[]')
-  $scope.model.selected = JSON.parse($window.localStorage.selected || '[]')
-
-  var read = {}
-  $scope.model.read.forEach(function(item) {
-    read[item.id] = true
-  })
-  var time = new Date().getTime()
-  $scope.model.all = $scope.model.all.filter(function(item) {
-    return config.item.getDate(item).getTime() < time
-  }).filter(function(item) {
-    return !read[item.id]
-  })
+  if ($window.localStorage.all) {
+    $scope.filterAll(JSON.parse($window.localStorage.all))
+  } else {
+    $http.get(config.api.all).then(function(response) {
+      $scope.filterAll(response.data.items)
+      $window.localStorage.all = JSON.stringify(response.data.items)
+    })
+  }
 })
 
 .controller('DashCtrl', function($scope) {
