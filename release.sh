f6c725c19bac14d2d61affb89ac420f7dd9a8ffd
Taras Serdyuk <taras@techburg.io>
2015-11-05 18:16:56 +0300

    fixed release script

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd ionic
 cordova build --release android
 cd platforms/android/build/outputs/apk
 jarsigner -storepass password -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
