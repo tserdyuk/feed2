01c184b817c61f314a56c3a2dcf42dbf43bedf23
Taras Serdyuk <taras@techburg.io>
2015-11-05 15:50:18 +0300

    building release version

---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cordova build --release android
+cd platforms/android/build/outputs/apk
+jarsigner -storepass password -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
+zipalign -f 4 android-release-unsigned.apk release.apk
