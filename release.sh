0a07e2769742814ff35162de9743cab31be278a1
Taras Serdyuk <taras@techburg.io>
2015-11-06 16:31:23 +0300

    minor

--- release.sh
+++ release.sh
@@ -3,4 +3,5 @@ cd ionic
 cordova build --release android
 cd platforms/android/build/outputs/apk
 jarsigner -storepass password -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
-zipalign -f 4 android-release-unsigned.apk release.apk
+zipalign -f 4 android-release-unsigned.apk feed.apk
+mv -f feed.apk ../../../../../../dist/
