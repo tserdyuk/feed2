2f1c42ffb61ec1adfe4c731fe7a9a3b30ed7c4ec
Taras Serdyuk <taras@techburg.io>
2015-11-06 15:15:12 +0300

    rename all to items

--- src/app/Main.js
+++ src/app/Main.js
@@ -10,12 +10,12 @@ export default function($scope, $window, $http) {
 	$scope.readMap = _.indexBy($scope.model.read, _.method('getId'))
 	$scope.isNew = item => !(item.getId() in $scope.readMap)
 	$scope.isRecent = item => item.getTime() < new Date().getTime()
-	if ($window.localStorage.all) {
-		$scope.model.all = JSON.parse($window.localStorage.all).map(Item)
+	if ($window.localStorage.items) {
+		$scope.model.items = JSON.parse($window.localStorage.items).map(Item)
 	} else {
 		$http.get(api.all).then(function(response) {
-			$scope.model.all = response.data.items.map(Item)
-			$window.localStorage.all = JSON.stringify(response.data.items)
+			$scope.model.items = response.data.items.map(Item)
+			$window.localStorage.items = JSON.stringify(response.data.items)
 		})
 	}
 }
