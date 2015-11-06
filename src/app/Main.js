bff11dcaf42d6422dfcc0e740be0ccf464dee100
Taras Serdyuk <taras@techburg.io>
2015-11-06 13:51:48 +0300

    recent auto filtration

--- src/app/Main.js
+++ src/app/Main.js
@@ -8,20 +8,13 @@ export default function($scope, $window, $http) {
 		selected: JSON.parse($window.localStorage.selected || '[]')
 	}
 	$scope.readMap = indexBy($scope.model.read, config.item.id)
-	$scope.isNew = item => {
-		return !(item[config.item.id] in $scope.readMap)
-	}
-	$scope.filterAll = function(items) {
-		var time = new Date().getTime()
-		$scope.model.all = items.filter(function(item) {
-			return config.item.getDate(item).getTime() < time
-		})
-	}
+	$scope.isNew = item => !(item[config.item.id] in $scope.readMap)
+	$scope.isRecent = item => config.item.getDate(item).getTime() < new Date().getTime()
 	if ($window.localStorage.all) {
-		$scope.filterAll(JSON.parse($window.localStorage.all))
+		$scope.model.all = JSON.parse($window.localStorage.all)
 	} else {
 		$http.get(config.api.all).then(function(response) {
-			$scope.filterAll(response.data.items)
+			$scope.model.all = response.data.items
 			$window.localStorage.all = JSON.stringify(response.data.items)
 		})
 	}
