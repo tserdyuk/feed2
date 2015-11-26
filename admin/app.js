4b3fb135e8d62f6969c14c9d788b58e0bdad13fe
Taras Serdyuk <taras@techburg.io>
2015-11-26 17:05:28 +0300

    admin post editing routing

--- admin/app.js
+++ admin/app.js
@@ -1,4 +1,5 @@
 import angular from 'angular'
+import _ from 'lodash'
 
 angular.module('feed-admin', [
 	require('angular-ui-router')
@@ -6,37 +7,36 @@ angular.module('feed-admin', [
 
 .config(($stateProvider, $urlRouterProvider) => {
 	$stateProvider
-		.state('posts', {
-			url: '/posts',
-			templateUrl: 'posts.html',
-			controller: 'Posts'
-		})
-		.state('post', {
-			url: '/post',
-			templateUrl: 'post.html',
-			controller: 'Post'
-		})
+	.state('posts', {
+		url: '/posts', templateUrl: 'posts.html', controller: 'Posts'
+	}).state('new-post', {
+		url: '/post', templateUrl: 'post.html', controller: 'Post'
+	}).state('edit-post', {
+		url: '/post/:id', templateUrl: 'post.html', controller: 'Post'
+	})
 	$urlRouterProvider.otherwise('/posts')
 })
 
 .controller('Main', function($scope) {
+	$scope.posts = [{
+		id: '1',
+		title: '1',
+		delay: '1'
+	}, {
+		id: '2',
+		title: '2',
+		delay: '2'
+	}]
 })
 
 .controller('Posts', ($scope) => {
 	const s = $scope
-	s.items = [{
-		title: '1',
-		delay: '1'
-	}, {
-		title: '2',
-		delay: '2'
-	}]
-	s.edit = (post) => {
-	}
 	s.remove = (post) => {
 	}
 })
 
-.controller('Post', ($scope) => {
-	alert('Post!')
+.controller('Post', ($scope, $stateParams) => {
+	const s = $scope
+	s.post = _.find(s.posts, 'id', $stateParams.id)
+	console.log($stateParams.id, s.post)
 })
